643199558b5d8ec141999900523942e8880aa1cc
UnYan <415212688@qq.com>
2021-01-17 16:09:46 +0800

    1.1

--- src/main/java/miniplc0java/App.java
+++ src/main/java/miniplc0java/App.java
@@ -1,3 +1,24 @@
+package miniplc0java;
+
+import miniplc0java.analyser.Analyser;
+import miniplc0java.tokenizer.StringIter;
+import miniplc0java.tokenizer.Tokenizer;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File(args[0]));
+        StringIter it = new StringIter(sc);
+        Tokenizer tokenizer = new Tokenizer(it);
+        Analyser analyser = new Analyser(tokenizer);
+        analyser.analyseProgram(args[1]);
+    }
+}
+
+
+
 //package miniplc0java;
 //
 //import java.io.File;
